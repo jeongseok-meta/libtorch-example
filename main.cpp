50edad877d460b83e05c11bd404b8022da757fee
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-03-15 10:44:34 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <torch/csrc/jit/python/python_ivalue.h>
+
+int main() {
+  using torch::autograd::AutogradContext;
+  using torch::autograd::variable_list;
+
+  auto vl = variable_list();
+  auto ac = AutogradContext();
+
+  (void)vl;
+  (void)ac;
+  
+  return 0;
+}
