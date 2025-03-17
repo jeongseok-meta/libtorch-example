2b600804d292f6d704169a6cb9f64de75e5f2e84
Jeongseok Lee <jeongseok@meta.com>
2025-03-17 10:25:50 -0700

    Fix link issue with libpython313.lib

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <torch/csrc/jit/python/python_ivalue.h>
 
 int main() {
-  // using torch::autograd::AutogradContext;
   using torch::autograd::variable_list;
-
   auto vl = variable_list();
-  // auto ac = AutogradContext();
-
   (void)vl;
-  // (void)ac;
   
   return 0;
 }
