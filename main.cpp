13677f5da648c4b39e24528ffba8619b1f29cd55
Jeongseok Lee <jeongseok@meta.com>
2025-03-17 09:53:57 -0700

    Add pytorch to pixi environment

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <torch/csrc/jit/python/python_ivalue.h>
 
 int main() {
-  using torch::autograd::AutogradContext;
+  // using torch::autograd::AutogradContext;
   using torch::autograd::variable_list;
 
   auto vl = variable_list();
-  auto ac = AutogradContext();
+  // auto ac = AutogradContext();
 
   (void)vl;
-  (void)ac;
+  // (void)ac;
   
   return 0;
 }
